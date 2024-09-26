c335637a5141037167b7ef67e6733c1ad2a671a4
FEbikaji8441 <febikajicvn11b@gmail.com>
2024-09-27 02:10:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "dbpassword",
     database: 'Jawan'  // Use the "Jawan" database
 });
 
